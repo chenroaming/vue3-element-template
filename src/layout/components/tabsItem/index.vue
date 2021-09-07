4e3aa9de126a3c06f81880d528378113cd697e44
chenxiaoming <chenxiaomingLS@jomoo.cn>
2021-09-07 11:51:26 +0800

    【feat】:1.完善tabs菜单栏组件功能。2.组件库因beta版本bug降级。

--- src/layout/components/tabsItem/index.vue
+++ src/layout/components/tabsItem/index.vue
@@ -2,20 +2,45 @@
   <div class="tabs">
     <el-button type="text" @click="collaspse">{{ status }}</el-button>
     <div class="tabs-container" ref="container">
-      <div class="tabs-container-inner" ref="inner" :style="{ left: dynamicLeft + 'px' }">
+      <el-icon
+        style="cursor: pointer;z-index:99;background: #fff;"
+        @click="move('left')"
+        :size="size"
+        :color="color"
+      >
+        <ArrowLeft />
+      </el-icon>
+      <div
+        class="tabs-container-inner"
+        ref="inner"
+        :style="{ left: dynamicLeft + 'px' }"
+      >
         <el-tag
           class="tabs-container-item"
           v-for="(item, i) in tabsMenus"
           :key="item.name"
-          :ref="item => { if (item) tags[i] = item }"
+          :ref="
+            item => {
+              if (item) tags[i] = item;
+            }
+          "
           :effect="isChoise(item.path)"
           @click="handleClick(item.path, i)"
           @close="handleClose(item.path)"
           :closable="closable"
           size="mini"
-          >{{ item.meta.title }}</el-tag>
+          >{{ item.meta.title }}</el-tag
+        >
       </div>
     </div>
+    <el-icon
+      style="cursor: pointer;"
+      @click="move('right')"
+      :size="size"
+      :color="color"
+    >
+      <ArrowRight />
+    </el-icon>
   </div>
 </template>
 
@@ -24,8 +49,15 @@ import { useStore } from 'vuex'
 import { computed, ref, onBeforeUpdate, watch } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import { last } from 'lodash'
+import { ArrowLeft, ArrowRight } from '@element-plus/icons'
 export default {
+  components: {
+    ArrowLeft,
+    ArrowRight
+  },
   setup () {
+    const size = ref(20)
+    const color = ref('#aaa')
     const { getters, state, dispatch, commit } = useStore()
     const $route = useRoute()
     const $router = useRouter()
@@ -36,6 +68,15 @@ export default {
     const inner = ref(null)
     const tags = ref([])
     const dynamicLeft = ref(0)
+    const move = type => {
+      if (type === 'left' && dynamicLeft.value >= 0) return false
+      const containerWidth = container.value.clientWidth // 外部盒子的长度
+      const innerWidth = inner.value.clientWidth // 内部盒子的长度
+      if (type === 'right' && innerWidth < containerWidth) return false
+      if (type === 'right' && innerWidth - containerWidth < -dynamicLeft.value) { return false }
+      type === 'left' && (dynamicLeft.value += 30)
+      type === 'right' && (dynamicLeft.value -= 30)
+    }
     const tabsMenus = computed(() => {
       return state.app.tabsMenus
     })
@@ -56,7 +97,7 @@ export default {
       }
     }
     // 计算当前点击的tabs标签的总长度
-    const countTotalWidth = (i) => {
+    const countTotalWidth = i => {
       const curArr = tags.value.slice(0, i + 1)
       const totalWidth = curArr.reduce((acc, cur) => {
         return acc + cur.$el.clientWidth + 20
@@ -64,27 +105,32 @@ export default {
       return totalWidth
     }
     // 计算当前点击的tabs标签的长度
-    const countWidth = (i) => {
+    const countWidth = i => {
       const width = i < tags.value.length ? tags.value[i].$el.clientWidth : 0
       return width
     }
-    const handleClick = (itemPath) => {
+    const handleClick = itemPath => {
       $router.replace(itemPath)
     }
     // 是否不在可视区域内
-    const isHide = (index) => {
+    const isHide = index => {
       // 判断是否在可视区域左侧
-      if (-(dynamicLeft.value) > countTotalWidth(index)) {
+      if (-dynamicLeft.value > countTotalWidth(index)) {
         return {
           type: 'hideInLeft',
-          diff: -(dynamicLeft.value) - countTotalWidth(index)
+          diff: -dynamicLeft.value - countTotalWidth(index)
         }
       }
       // 判断是否在可视区域右侧
-      if (-(dynamicLeft.value) + container.value.clientWidth < countTotalWidth(index)) {
+      if (
+        -dynamicLeft.value + container.value.clientWidth <
+        countTotalWidth(index)
+      ) {
         return {
           type: 'hideInRight',
-          diff: countTotalWidth(index) - (-(dynamicLeft.value) + container.value.clientWidth)
+          diff:
+            countTotalWidth(index) -
+            (-dynamicLeft.value + container.value.clientWidth)
         }
       }
       // 在可视区域内
@@ -93,7 +139,7 @@ export default {
         diff: 0
       }
     }
-    const moveTabs = (index) => {
+    const moveTabs = index => {
       const { type, diff } = isHide(index)
       const containerWidth = container.value.clientWidth // 外部盒子的长度
       // 当点击的是第一个或者第二个时，则直接将移动的距离置为0
@@ -106,13 +152,19 @@ export default {
         return
       }
       // 计算当前点击的tab距离右边的距离是否小于下一个tab的距离
-      if (containerWidth - (countTotalWidth(index) + dynamicLeft.value) <= countWidth(index + 1)) {
+      if (
+        containerWidth - (countTotalWidth(index) + dynamicLeft.value) <=
+        countWidth(index + 1)
+      ) {
         // 计算该tag和下一个tag的长度之和，20是margin的宽度
         dynamicLeft.value -= countWidth(index) + countWidth(index + 1) + 20
         return
       }
       // 计算当前点击的tab与父容器偏移值与偏移值之和是否小于本标签长度
-      if (tags.value[index].$el.offsetLeft + dynamicLeft.value < countWidth(index)) {
+      if (
+        tags.value[index].$el.offsetLeft + dynamicLeft.value <
+        countWidth(index)
+      ) {
         // 计算该tag和上一个tag的长度之和，20是margin的宽度
         dynamicLeft.value += countWidth(index) + countWidth(index - 1) + 20
       }
@@ -137,7 +189,10 @@ export default {
       container,
       inner,
       tags,
-      dynamicLeft
+      dynamicLeft,
+      size,
+      color,
+      move
     }
   }
 }
@@ -166,7 +221,7 @@ export default {
       display: flex;
       align-items: center;
       position: relative;
-      transition: left .3s;
+      transition: left 0.3s;
     }
   }
 }
